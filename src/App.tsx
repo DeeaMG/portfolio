6549a66023149cea11800f97d95748dec94e668a
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-09-06 18:21:07 +0300

    Outlined the idea for the portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import Profile from "./components/Profile/Profile";
+
+/*
+  - profile photo + socials + CV
+  - Experience
+  - Certifications + Education
+  - Projects
+  - Others related to this(articles written)
+*/
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>Ganea Andreea</header>
+      <Profile />
     </div>
   );
 }
