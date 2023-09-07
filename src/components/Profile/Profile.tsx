9b61f7da88a2633691ce297a2ddb59fff5237dbf
Ganea Andreea Maria <ganea.andreea.maria@gmail.com>
2023-09-07 18:32:34 +0300

    Updated logo Added button for CV Added styles for logo and button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,22 +5,24 @@ import logo from "../../assets/logo.png";
 function Profile() {
   return (
     <div>
-      <div className="about-me">
-        <p className="description">
-          <h2>Designing the Web with Code & Creativity: Ganea Andreea's Digital Playground</h2>
+      <div className='logo-container'>
+        <img src={logo} alt='Logo' className='logo' />
+      </div>
+      <div className='about-me-container'>
+        <p className='description'>
+          <h2>Designing the Web with Code & Creativity</h2>
           <h5>
             Hi there! I'm Ganea Andreea, an experienced Web Developer with over 2 years under my
-            belt, and an aspiring Graphic Designer ready to make my mark. Welcome to my creative
-            world!
+            belt. Welcome to my creative world!
           </h5>
+          <div className='button-container'>
+            <button className='cv-button'>Know me better</button>
+          </div>
         </p>
 
-        <img src={profile} alt="Profile image" className="profile-image" />
+        <img src={profile} alt='Profile image' className='profile-image' />
       </div>
-      <div>
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <div className="footer">
+      <div className='footer-container'>
         <h2>Footer + Socials</h2>
       </div>
     </div>
