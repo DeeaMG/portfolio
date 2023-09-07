23caa9bc2f46d86fa5b355b38cf7182c06d668f6
Ganea Andreea <deeaganea@gmail.com>
2023-09-07 22:41:06 +0300

    Created the download functionality TBD: Moving the pdf file on a server

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,26 +3,37 @@ import profile from "../../assets/profile-photo.jpg";
 import logo from "../../assets/logo.png";
 
 function Profile() {
+  const onDownloadCV = () => {
+    const cvPath: string = process.env.PUBLIC_URL + "assets/Ganea_Andreea_Maria.pdf";
+
+    const anchor: HTMLAnchorElement = document.createElement("a");
+    anchor.href = cvPath;
+    anchor.download = "Ganea_Andreea_Maria.pdf";
+    anchor.click();
+  };
+
   return (
     <div>
-      <div className='logo-container'>
-        <img src={logo} alt='Logo' className='logo' />
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
       </div>
-      <div className='about-me-container'>
-        <p className='description'>
-          <h2>Designing the Web with Code & Creativity</h2>
-          <h5>
+      <div className="about-me-container">
+        <div className="description">
+          <h1>Designing the Web with Code & Creativity</h1>
+          <h4>
             Hi there! I'm Ganea Andreea, an experienced Web Developer with over 2 years under my
             belt. Welcome to my creative world!
-          </h5>
-          <div className='button-container'>
-            <button className='cv-button'>Know me better</button>
+          </h4>
+          <div className="button-container">
+            <button className="cv-button" onClick={onDownloadCV}>
+              Know me better
+            </button>
           </div>
-        </p>
+        </div>
 
-        <img src={profile} alt='Profile image' className='profile-image' />
+        <img src={profile} alt="Profile" className="profile-image" />
       </div>
-      <div className='footer-container'>
+      <div className="footer-container">
         <h2>Footer + Socials</h2>
       </div>
     </div>
